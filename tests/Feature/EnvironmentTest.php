b0da5b6a8c58f779dbad61d094f45c55d9a8de1d
AdaliCode <fiezayyan@gmail.com>
2024-05-09 14:05:08 +0700

    membuat file laravel dan menerapkan test environment

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testEnv()
+    {
+        $appName = env("WEB");
+        self::assertEquals("Aflix", $appName);
+    }
+    public function testDefaultValue()
+    {
+        $author = env("AUTHOR", "Ariiq");
+        self::assertEquals("Ariiq", $author);
+    }
+    public function testEnvironment()
+    {
+        if (App::environment("testing")) {
+            echo "LOGIC IN TESTING ENV\n";
+            self::assertTrue(true);
+        }
+    }
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
